27b7d77d1ea89cc3e884c91567903938965c939d
tubar02 <toszerodois@gmail.com>
2025-09-05 00:29:59 -0300

    Prepara as pastas para a resolução do exemplo do capítulo 5.

---
+++ Capítulo 5/Exemplo/metodos_numericos_0.2.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Solver():
+	def __init__(self, eq_dif = None, parametros = None, metodo = None, precisao = None, iteracoes = None):
+		self.eq_dif = eq_dif
+		self.parametros = parametros
+		self.metodo = metodo
+		self.precisao = precisao
+		self.iteracoes = iteracoes
+
+	def set_eq_dif(self, eq_dif):
+		self.eq_dif = eq_dif
+
+	def set_parametros(self, parametros):
+		self.parametros = parametros
+
+	def set_metodo(self, metodo):
+		self.metodo = metodo
+
+	def set_precisao(self, precisao):
+		self.precisao = precisao
+
+	def set_iteracoes(self, iteracoes):
+		self.iteracoes = iteracoes
+
+	def solve(self):
+		dados = Data(self)
+		dados.calcula()
+		return dados.solved
+
+class Data():
+	def __init__(self, solver):
+		self.solver = solver
+
+		if solver.eq_dif == "Nuclear Decay (EDO)":
+			self.solved = np.zeros((2, self.solver.iteracoes))
+			self.solved[0][0] = self.solver.parametros["NU_0"]
+
+	def calcula(self):
+		if self.solver.eq_dif == "Nuclear Decay (EDO)":
+			self.nuclear_decay()
+
+	def nuclear_decay(self):
+		if self.solver.metodo == "Euler":
+			for i in range(1, self.solver.iteracoes):
+				self.solved[0][i] = self.solved[0][i - 1] - (self.solved[0][i - 1] / self.solver.parametros["tau"]) * self.solver.precisao
+				self.solved[1][i] = self.solved[1][i - 1] + self.solver.precisao
+
+def main():
+	soluciona = Solver()
+	soluciona.set_eq_dif("Nuclear Decay (EDO)")
+	soluciona.set_parametros({"NU_0": 100, "tau": 1})
+	soluciona.set_metodo("Euler")
+	soluciona.set_precisao(0.05)
+	soluciona.set_iteracoes(100)
+
+	solved = soluciona.solve()
+	NU, t = solved[0], solved[1]
+
+	plt.plot(t, NU, "b-", label="Decaimento Nuclear (Euler)")
+	plt.xlabel("Tempo")
+	plt.ylabel("Número de núcleos")
+	plt.title("Decaimento Nuclear via Euler")
+	plt.legend()
+	plt.grid(True)
+	plt.show()
+
+
+
+if __name__ == "__main__":
+	main()
